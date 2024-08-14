71d15917340a02c8223fad87b9007ca94cffa0b7
dineshkumar <65344594+dineshunbots@users.noreply.github.com>
2024-08-14 18:56:28 +0530

    up

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-  connectionLimit: process.env.CONNECTION_LIMIT,    // the number of connections node.js will hold open to our database
+  connectionLimit: process.env.CONNECTION_LIMIT,   
   password: process.env.DB_PASS,
   user: process.env.DB_USER,
   database: process.env.MYSQL_DB,
